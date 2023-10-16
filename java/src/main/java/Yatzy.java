1dd818516f227f6da111c0762509712fdf725471
moez <khamassi_moez@yahoo.fr>
2023-10-16 09:47:43 +0200

    enhance chance

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -12,13 +12,7 @@ public class Yatzy {
     }
 
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return d1 + d2 + d3 + d4 + d5;
     }
 
     public static int yatzy(int... dice) {
